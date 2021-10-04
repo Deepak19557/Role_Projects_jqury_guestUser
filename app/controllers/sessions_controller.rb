1565c8292b50663b1bfdf5c65a547c490cde280a
Deepak19557 <chourasiya123deepak@gmail.com>
2021-10-04 15:42:26 +0530

    add_role

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < Devise::SessionsController
 				sign_in @user
 				redirect_to root_path
 			else
-				User.create(email: params[:email], role: params[:role], password:"123456")
+				User.create(email: params[:email], role: params[:role], name: params[:name], password:"123456")
 				sign_in @user
 				redirect_to root_path
 			end
